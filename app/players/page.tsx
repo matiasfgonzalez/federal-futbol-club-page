fd64309bb80f35fb5c461b99175372226e1f058d
Mat Gonzalez <matiasgonzalez.652@gmail.com>
2024-08-27 20:55:24 -0300

    Players Page.

--- app/players/page.tsx
+++ app/players/page.tsx
@@ -1,84 +1,119 @@
-import "@/app/players/style.css";
+"use client";
+import { useState } from "react";
+import { players } from "@/data/players";
 
-const page = () => {
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
+import HeaderNavFechas from "@/components/components-ui/header-nav-fechas";
+
+const PlayerPage = () => {
+  const [flippedCard, setFlippedCard] = useState<number | null>(null);
+
+  const handleCardClick = (playerId: number) => {
+    setFlippedCard(flippedCard === playerId ? null : playerId);
+  };
+
   return (
-    <main>
-      <div className="fut-player-card">
-        <div className="player-card-top">
-          <div className="player-master-info">
-            <div className="player-rating">
-              <span>97</span>
-            </div>
-            <div className="player-position">
-              <span>RW</span>
-            </div>
-            <div className="player-nation">
-              <img
-                src="https://selimdoyranli.com/cdn/fut-player-card/img/argentina.svg"
-                alt="Argentina"
-                draggable="false"
-              />
-            </div>
-            <div className="player-club">
-              <img
-                src="/img/escudo-federal-futbol-club-sin-fondo.webp"
-                alt="Federal Futbol Club"
-                draggable="false"
-              />
-            </div>
-          </div>
-          <div className="player-picture">
-            <img
-              src="https://selimdoyranli.com/cdn/fut-player-card/img/messi.png"
-              alt="Messi"
-              draggable="false"
-            />
-            <div className="player-extra">
-              <span>4*SM</span>
-              <span>4*WF</span>
-            </div>
-          </div>
-        </div>
-        <div className="player-card-bottom">
-          <div className="player-info">
-            <div className="player-name">
-              <span>MESSI</span>
-            </div>
-            <div className="player-features">
-              <div className="player-features-col">
-                <span>
-                  <div className="player-feature-value">97</div>
-                  <div className="player-feature-title">PAC</div>
-                </span>
-                <span>
-                  <div className="player-feature-value">95</div>
-                  <div className="player-feature-title">SHO</div>
-                </span>
-                <span>
-                  <div className="player-feature-value">0</div>
-                  <div className="player-feature-title">PAS</div>
-                </span>
-              </div>
-              <div className="player-features-col">
-                <span>
-                  <div className="player-feature-value">99</div>
-                  <div className="player-feature-title">DRI</div>
-                </span>
-                <span>
-                  <div className="player-feature-value">35</div>
-                  <div className="player-feature-title">DEF</div>
-                </span>
-                <span>
-                  <div className="player-feature-value">68</div>
-                  <div className="player-feature-title">PHY</div>
-                </span>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
+    <>
+      <HeaderNavFechas />
+      <main className="flex-grow flex items-center justify-center bg-gradient-to-b from-primary/20 to-primary/5 p-4 bg-[#1b2f62]">
+        <Carousel className="w-full max-w-md mx-auto">
+          <CarouselContent>
+            {players.map((player) => (
+              <CarouselItem
+                key={player.id}
+                className="md:basis-11/12 lg:basis-10/12"
+              >
+                <div className="p-1">
+                  <Card
+                    style={{
+                      backgroundImage: `url(${player.imgBody})`,
+                      backgroundSize: "cover",
+                      backgroundRepeat: "no-repeat",
+                      backgroundPosition: "center",
+                    }}
+                    className={`cursor-pointer transition-transform duration-500 ${
+                      flippedCard === player.id
+                        ? "[transform:rotateY(180deg)]"
+                        : ""
+                    }`}
+                    onClick={() => handleCardClick(player.id)}
+                  >
+                    <CardContent className="flex flex-col items-center justify-center p-6 h-[500px]">
+                      <div className="w-full h-full flex flex-col justify-between">
+                        <div
+                          className={`flex-grow ${
+                            flippedCard === player.id
+                              ? "[transform:rotateY(180deg)]"
+                              : ""
+                          }`}
+                        >
+                          {flippedCard !== player.id && (
+                            // Frente de la tarjeta
+                            <>
+                              <Avatar className="w-24 h-24 mx-auto mb-4 bg-white">
+                                <AvatarImage
+                                  src={player.imgFace + `?height=96&width=96`}
+                                  alt={player.name}
+                                />
+                                <AvatarFallback>
+                                  {player.name
+                                    .split(" ")
+                                    .map((n) => n[0])
+                                    .join("")}
+                                </AvatarFallback>
+                              </Avatar>
+                            </>
+                          )}
+                        </div>
+                        <div className="text-center text-[#1b2f62] p-2 bg-white opacity-75 rounded-md">
+                          {flippedCard !== player.id ? (
+                            <>
+                              <h3 className="text-lg font-semibold text-center mb-2 text-[#1b2f62]">
+                                {player.name}
+                              </h3>
+                              <p className="text-sm text-center text-[#1b2f62]">
+                                {player.position}
+                              </p>
+                              <div className="flex justify-center">
+                                <img
+                                  src="/img/escudo-federal-futbol-club-sin-fondo.webp?height=50&width=50"
+                                  alt="Federal FC"
+                                  className="h-12 text-center"
+                                />
+                              </div>
+                            </>
+                          ) : (
+                            <div className=" [transform:rotateY(180deg)]">
+                              <h3 className="text-lg font-semibold text-center  mb-2  text-[#1b2f62]">
+                                {player.name}
+                              </h3>
+                              <p className="text-sm text-center mb-1 text-[#1b2f62]">
+                                {player.country}
+                              </p>
+                            </div>
+                          )}
+                        </div>
+                      </div>
+                    </CardContent>
+                  </Card>
+                </div>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+          <CarouselPrevious variant="ghost" />
+          <CarouselNext variant="ghost" />
+        </Carousel>
+      </main>
+    </>
   );
 };
 
-export default page;
+export default PlayerPage;
---
+++ data/players.ts
@@ -0,0 +1,27 @@
+// Datos de ejemplo para los jugadores
+export const players = [
+  {
+    id: 1,
+    name: "Kevin Boujon",
+    position: "Centrocampista",
+    country: "Concepción del Uruguay",
+    imgFace: "/img/face/kevin-sin-fondo-face.webp",
+    imgBody: "/img/body/kevin.webp",
+  },
+  {
+    id: 2,
+    name: "Brian Sosa",
+    position: "Lateral",
+    country: "Villaguay",
+    imgFace: "/img/face/brian-sin-fondo-face.webp",
+    imgBody: "/img/body/brian.webp",
+  },
+  {
+    id: 3,
+    name: "Gonzalo Barsi",
+    position: "Central",
+    country: "******",
+    imgFace: "/img/face/gonza-sin-fondo-face.webp",
+    imgBody: "/img/body/gonza.webp",
+  },
+];

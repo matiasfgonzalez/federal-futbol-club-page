5f373044bfedbbe04b3dd3249475ea023b89fa2e
Mat Gonzalez <matiasgonzalez.652@gmail.com>
2024-08-28 00:18:14 -0300

    Prov 01

---
+++ app/page-test/page.tsx
@@ -0,0 +1,92 @@
+import "./style.css";
+
+const pageTest = () => {
+  return (
+    <div className="contenedorp">
+      <div className="contenedor">
+        <div className="tarjeta">
+          <div className="imgBx">
+            <img src="/img/testImg/brian.webp" alt="" />
+          </div>
+          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
+          <h2>
+            <img src="/img/escudo-federal-futbol-club-sin-fondo.webp" />
+            Brian
+          </h2>
+        </div>
+        <div className="tarjeta">
+          <div className="imgBx">
+            <img src="/img/testImg/brian.webp" alt="" />
+          </div>
+          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
+          <h2>
+            <img
+              src="/img/escudo-federal-futbol-club-sin-fondo.webp"
+              width={"50px"}
+              height={"50px"}
+            />
+            Brian
+          </h2>
+        </div>
+        <div className="tarjeta">
+          <div className="imgBx">
+            <img src="/img/testImg/brian.webp" alt="" />
+          </div>
+          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
+          <h2>Brian</h2>
+        </div>
+        <div className="tarjeta">
+          <div className="imgBx">
+            <img src="/img/testImg/brian.webp" alt="" />
+          </div>
+          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
+          <h2>Brian</h2>
+        </div>
+        <div className="tarjeta">
+          <div className="imgBx">
+            <img src="/img/testImg/brian.webp" alt="" />
+          </div>
+          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
+          <h2>Brian</h2>
+        </div>
+        <div className="tarjeta">
+          <div className="imgBx">
+            <img src="/img/testImg/brian.webp" alt="" />
+          </div>
+          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
+          <h2>Brian</h2>
+        </div>
+        <div className="tarjeta">
+          <div className="imgBx">
+            <img src="/img/testImg/brian.webp" alt="" />
+          </div>
+          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
+          <h2>Brian</h2>
+        </div>
+        <div className="tarjeta">
+          <div className="imgBx">
+            <img src="/img/testImg/brian.webp" alt="" />
+          </div>
+          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
+          <h2>Brian</h2>
+        </div>
+        <div className="tarjeta">
+          <div className="imgBx">
+            <img src="/img/testImg/brian.webp" alt="" />
+          </div>
+          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
+          <h2>Brian</h2>
+        </div>
+        <div className="tarjeta">
+          <div className="imgBx">
+            <img src="/img/testImg/brian.webp" alt="" />
+          </div>
+          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
+          <h2>Brian</h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default pageTest;

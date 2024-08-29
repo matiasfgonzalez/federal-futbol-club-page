34f56ce5f433c015cbf6bd9ff91d3ded3552bbf6
Mat Gonzalez <matiasgonzalez.652@gmail.com>
2024-08-29 19:11:49 -0300

    Pagina de prueba para publicar jugadores.

--- app/page-test/page.tsx
+++ app/page-test/page.tsx
@@ -1,91 +1,38 @@
 import "./style.css";
 
 const pageTest = () => {
+  const jugadores = [
+    {
+      name: "Ángel Di María",
+      imgBody: "/img/body/di-maria.webp",
+      imgBodySF: "/img/body/di-maria2.webp",
+    },
+    {
+      name: "Emiliano Martínez",
+      imgBody: "/img/body/dibu.webp",
+      imgBodySF: "/img/body/dibu2.webp",
+    },
+  ];
+
   return (
-    <div className="contenedorp">
-      <div className="contenedor">
-        <div className="tarjeta">
-          <div className="imgBx">
-            <img src="/img/testImg/brian.webp" alt="" />
+    <>
+      <h1 className="text-center pt-5 pb-10">JUGADORES</h1>
+      <div className="contenedorp">
+        <div className="contenedor">
+          <div className="grid grid-cols-1 gap-12 md:grid-cols-2 lg:grid-cols-2">
+            {jugadores.map((j, index) => (
+              <div key={index} className="tarjeta shadow">
+                <div className="imgBx">
+                  <img src={j.imgBody} alt="" />
+                </div>
+                <img src={j.imgBodySF} className="c3d  img-sf" alt="" />
+                <h2>{j.name}</h2>
+              </div>
+            ))}
           </div>
-          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
-          <h2>
-            <img src="/img/escudo-federal-futbol-club-sin-fondo.webp" />
-            Brian
-          </h2>
-        </div>
-        <div className="tarjeta">
-          <div className="imgBx">
-            <img src="/img/testImg/brian.webp" alt="" />
-          </div>
-          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
-          <h2>
-            <img
-              src="/img/escudo-federal-futbol-club-sin-fondo.webp"
-              width={"50px"}
-              height={"50px"}
-            />
-            Brian
-          </h2>
-        </div>
-        <div className="tarjeta">
-          <div className="imgBx">
-            <img src="/img/testImg/brian.webp" alt="" />
-          </div>
-          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
-          <h2>Brian</h2>
-        </div>
-        <div className="tarjeta">
-          <div className="imgBx">
-            <img src="/img/testImg/brian.webp" alt="" />
-          </div>
-          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
-          <h2>Brian</h2>
-        </div>
-        <div className="tarjeta">
-          <div className="imgBx">
-            <img src="/img/testImg/brian.webp" alt="" />
-          </div>
-          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
-          <h2>Brian</h2>
-        </div>
-        <div className="tarjeta">
-          <div className="imgBx">
-            <img src="/img/testImg/brian.webp" alt="" />
-          </div>
-          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
-          <h2>Brian</h2>
-        </div>
-        <div className="tarjeta">
-          <div className="imgBx">
-            <img src="/img/testImg/brian.webp" alt="" />
-          </div>
-          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
-          <h2>Brian</h2>
-        </div>
-        <div className="tarjeta">
-          <div className="imgBx">
-            <img src="/img/testImg/brian.webp" alt="" />
-          </div>
-          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
-          <h2>Brian</h2>
-        </div>
-        <div className="tarjeta">
-          <div className="imgBx">
-            <img src="/img/testImg/brian.webp" alt="" />
-          </div>
-          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
-          <h2>Brian</h2>
-        </div>
-        <div className="tarjeta">
-          <div className="imgBx">
-            <img src="/img/testImg/brian.webp" alt="" />
-          </div>
-          <img src="/img/testImg/brian-sin-fondo.png" className="c3d" alt="" />
-          <h2>Brian</h2>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
